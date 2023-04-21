41f613df97f9de6fa576b65670c7aeb984e9ebb3
username5012 <118868860+username5012@users.noreply.github.com>
2023-04-21 19:09:35 +0900

    [level 0] Title: 이진수 더하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.py
+++ 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.py
@@ -1,8 +1,12 @@
 def solution(bin1, bin2):
     answer = ''
+    # 2진수를 10진수로 바꿔주기
     bin1 = int(bin1, 2)
     bin2 = int(bin2, 2)
     
+    # 10진수로 바꾼 두 수를 더하고 2진수로 바꾸기
     answer = bin(bin1 + bin2)
+    
+    # 파이썬 2진수 변환 시 생기는 앞의 '0b' 제거
     answer = answer[2:]
     return answer
