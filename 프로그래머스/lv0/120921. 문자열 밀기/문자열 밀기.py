19f451b5042a7c79024d642bb45c1863a725947c
username5012 <118868860+username5012@users.noreply.github.com>
2023-04-24 21:08:03 +0900

    [level 0] Title: 문자열 밀기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.py
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.py
@@ -1,11 +1,13 @@
 def solution(A, B):
     answer = -1
     
+# C => A를 i만큼 이동했을때 나오는 단어
     for i in range(len(A)):
         C = ''
         C += A[-i:]
         C += A[:-i]
         
+# A를 i만큼 움직인 C가 B와 같을 경우, answer는 i.
         if C == B:
             answer = i
             break
