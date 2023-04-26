aa616235f3bf034a6287fe8659051d812ce429e8
username5012 <118868860+username5012@users.noreply.github.com>
2023-04-26 18:05:20 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.py
+++ 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.py
@@ -1,11 +1,15 @@
 def solution(chicken):
     answer = 0
-    coup = chicken
+
+    coupon = chicken  # 쿠폰 = 여태 먹은 치킨의 양
     
-    while coup >= 10:
-        eaten = coup // 10 
-        answer += eaten 
-        coup = coup % 10 + eaten 
-    
+    while coupon >= 10: # 쿠폰이 10장 이상일 때까지 While문 실행.
+        
+        service = coupon // 10 # 서비스 치킨은 쿠폰 10장 당 1마리
+        answer += service
+        coupon = coupon % 10 + service # 쿠폰은 서비스 주문 시 사용하고 남은 쿠폰 + 서비스로 시킨 치킨 마리 수 
+        
+        # 쿠폰이 10장 미만으로 남을 때까지 루프
+        
 
     return answer
