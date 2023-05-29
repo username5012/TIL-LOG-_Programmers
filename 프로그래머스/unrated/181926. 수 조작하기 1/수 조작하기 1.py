d3242016e4ef9cc0a91a8743b49534b18e570b57
username5012 <118868860+username5012@users.noreply.github.com>
2023-05-30 02:48:38 +0900

    [unrated] Title: 수 조작하기 1, Time: 3.22 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.py
@@ -0,0 +1,14 @@
+def solution(n, control):
+    answer = n
+    
+    for i in range(len(control)):
+        if control[i] == 'w':
+            answer += 1
+        elif control[i] == 'a':
+            answer -= 10
+        elif control[i] == 's':
+            answer -= 1
+        elif control[i] == 'd':
+            answer += 10
+            
+    return answer
