94822b8544b2ad119ca639f2ad7bf06a87f68e9a
username5012 <118868860+username5012@users.noreply.github.com>
2023-06-12 16:43:55 +0900

    [unrated] Title: 수열과 구간 쿼리 1, Time: 6.35 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.py
@@ -0,0 +1,8 @@
+def solution(arr, queries):
+    answer = []
+    for query in queries:
+        s, e = query[0], query[1]
+        for i in range (s, e+1):
+            arr[i] += 1
+            
+    return arr
